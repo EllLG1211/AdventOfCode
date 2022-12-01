6f9e5a0f569e68463cac36878ae43bcd2a1753fe
Kevin <kevin.cuoq@touslesprix.com>
2022-12-01 10:27:33 +0100

    Part one successfully completed

---
+++ Day1/InputReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public class InputReader
+    {
+        private const string FILENAME = "input.txt";
+
+        public IEnumerable<int> GetInput()
+        {
+            List<int> values = new();
+            int tmp = 0;
+
+            string[] lines = File.ReadAllLines(FILENAME);
+            foreach(string line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    values.Add(tmp);
+                    tmp = 0;
+                }
+                else tmp += int.Parse(line);
+            }
+            return values;
+        }
+    }
+}
--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day1;
+
+Console.WriteLine("Advent of code, day 1");
+
+#region Part One
+Console.WriteLine("Part one: get the elf who carries the most calories\n");
+
+
+Console.WriteLine("Calories carried by each elf, in descending order:");
+var values = from element in new InputReader().GetInput() orderby element descending select element;
+foreach (var value in values) Console.WriteLine(value);
+
+Console.WriteLine();
+Console.WriteLine($"Elf with the most calories carries {values.First()} calories.");
+#endregion
+
