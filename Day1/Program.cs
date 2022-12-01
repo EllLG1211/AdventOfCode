a2d8f81e3d330632591bdf9b836bda44d63c0988
Kevin <kevin.cuoq@touslesprix.com>
2022-12-01 10:33:00 +0100

    Part two successfully completed.

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -10,7 +10,17 @@ Console.WriteLine("Calories carried by each elf, in descending order:");
 var values = from element in new InputReader().GetInput() orderby element descending select element;
 foreach (var value in values) Console.WriteLine(value);
 
-Console.WriteLine();
 Console.WriteLine($"Elf with the most calories carries {values.First()} calories.");
 #endregion
 
+Console.WriteLine();
+
+#region Part Two
+Console.WriteLine("Part two: get the total calories from the top three elves\n");
+
+Console.WriteLine("Top three elves:");
+var topThree = values.Take(3);
+foreach (var value in topThree) Console.WriteLine(value);
+
+Console.WriteLine($"The top three elves regroup {topThree.Sum()} calories.");
+#endregion
